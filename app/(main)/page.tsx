01b7ea8545d925a1e894ef4e8ada9859e83643fd
gokuljs <gokuljs029@gmail.com>
2024-04-06 12:46:59 +0530

    intial code

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -4,7 +4,7 @@ import getYear from '@/lib/getYear';
 import http from '@/lib/http';
 import Api from '@/lib/http';
 import Image from 'next/image';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 const IMAGE_URL = 'https://image.tmdb.org/t/p/w500/';
 // https://image.tmdb.org/t/p/w500/1E5baAaEse26fej7uHcjOgEE2t2.jpg
@@ -13,6 +13,9 @@ export default function Home(): JSX.Element {
   const [loading, setIsLoading] = useState(false);
   const [data, setData] = useState<Movie[]>([]);
   const [page, setPage] = useState(1);
+  const scrollContainerRef = useRef<HTMLDivElement>(null);
+  const lastScrollTopRef = useRef(0); // Ref to store the last scroll position
+
   useEffect(() => {
     const fetch = async () => {
       setIsLoading(true);
@@ -27,9 +30,34 @@ export default function Home(): JSX.Element {
     fetch();
   }, []);
 
+  useEffect(() => {
+    if (!scrollContainerRef || !scrollContainerRef.current) return;
+    const scrollContainer = scrollContainerRef.current;
+    const handleScroll = async () => {
+      const scrollTop = scrollContainer.scrollTop;
+      const scrollHeight = scrollContainer.scrollHeight;
+      const clientHeight = scrollContainer.clientHeight;
+      const scrolledPercentage = (scrollTop / (scrollHeight - clientHeight)) * 100;
+
+      const isForwardScroll = scrollTop > lastScrollTopRef.current;
+      if (isForwardScroll && scrolledPercentage > 50) {
+        console.log('more than 60');
+        const response = await http.get(`movie/popular?language=en-US&page=${page + 1}`);
+        setPage((page) => page + 1);
+        setData((data) => [...data, ...response.data.results]);
+      }
+
+      lastScrollTopRef.current = scrollTop;
+    };
+    scrollContainer.addEventListener('scroll', handleScroll);
+    return () => {
+      scrollContainer.removeEventListener('scroll', handleScroll);
+    };
+  }, [page]);
+
   console.log(data);
   return (
-    <div className="overflow-y-auto w-[100vw] h-full ">
+    <div ref={scrollContainerRef} className="overflow-y-auto w-[100vw] h-full bg-red-900">
       <div className="container mx-auto px-4 py-6">
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-6">
           {data.length > 0 &&
