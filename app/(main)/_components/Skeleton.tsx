69be36d9f8595038a1b7b68edabe436bef79d975
gokuljs <gokuljs029@gmail.com>
2024-04-07 18:30:49 +0530

    setup

--- app/(main)/_components/Skeleton.tsx
+++ app/(main)/_components/Skeleton.tsx
@@ -4,7 +4,7 @@ import { Skeleton } from '@/components/ui/skeleton';
 const SkeletonLoader = () => {
   return (
     <div className="w-full h-full py-3 overflow-auto grid grid-cols-1 lg:grid-cols-5 md:grid-cols-4 sm:grid-cols-3 xs:grid-cols-2 gap-6 justify-items-center mx-auto">
-      {Array.from({ length: 10 }, (_, index) => (
+      {Array.from({ length: 100 }, (_, index) => (
         <Skeleton key={index} className="w-[193px] h-[400px] rounded-lg" data-testid="skeleton" />
       ))}
     </div>
