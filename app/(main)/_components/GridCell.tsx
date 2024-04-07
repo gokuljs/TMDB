f79adf2e3deaf7c714c8507868d075a9d29ec34d
gokuljs <gokuljs029@gmail.com>
2024-04-07 12:52:58 +0530

    animation

--- app/(main)/_components/GridCell.tsx
+++ app/(main)/_components/GridCell.tsx
@@ -1,6 +1,7 @@
 import { Movie } from '@/lib/Models/movie';
 import getYear from '@/lib/getYear';
 import Image from 'next/image';
+import { useState } from 'react';
 
 interface DataProps {
   data: Movie[];
@@ -14,6 +15,8 @@ interface CellProps {
   data: DataProps;
 }
 export const Cell: React.FC<CellProps> = ({ columnIndex, rowIndex, style, data }) => {
+  const [isHovered, setIsHovered] = useState(false); // State to track hover state
+
   // Calculate the index of your item based on rowIndex and columnIndex
   const index = rowIndex * data.columnCount + columnIndex;
   const item = data.data[index];
@@ -23,10 +26,22 @@ export const Cell: React.FC<CellProps> = ({ columnIndex, rowIndex, style, data }
   }
 
   return (
-    <div className={`flex justify-center items-center fadeIn`} style={style} key={item.id.toString() + index}>
+    <div
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+      className={`flex justify-center items-center fadeIn`}
+      style={style}
+      key={item.id.toString() + index}
+    >
       <div title={item.title} className="bg-white  h-[360px] w-[193px] rounded-lg overflow-hidden shadow-lg flex flex-col">
         <div className="w-full h-[289px] relative">
-          <Image loading="eager" className="w-full h-full object-cover" src={`${IMAGE_URL}${item.poster_path}`} fill alt="movie title" />
+          <Image
+            loading="eager"
+            className={`w-full h-full object-cover transform ${isHovered ? 'scale-105' : 'scale-100'} transition-transform duration-300 ease-in-out`}
+            src={`${IMAGE_URL}${item.poster_path}`}
+            fill
+            alt="movie title"
+          />
         </div>
         <div className="p-4 bg-[#050E12] h-[calc(355px - 289px)] w-full flex flex-col">
           <p className="text-[#B6B6B6] text-lg font-medium overflow-hidden whitespace-nowrap overflow-ellipsis">{item.title}</p>
