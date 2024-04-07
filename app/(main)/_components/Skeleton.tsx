8e5f37f266b2ea49f8c27b7cb24a484e0c3c606a
gokuljs <gokuljs029@gmail.com>
2024-04-07 16:17:41 +0530

    skeleton

--- app/(main)/_components/Skeleton.tsx
+++ app/(main)/_components/Skeleton.tsx
@@ -4,16 +4,9 @@ import { Skeleton } from '@/components/ui/skeleton';
 const SkeletonLoader = () => {
   return (
     <div className="w-full h-full py-3 overflow-auto grid grid-cols-1 lg:grid-cols-5 md:grid-cols-4 sm:grid-cols-3 xs:grid-cols-2 gap-6 justify-items-center mx-auto">
-      <Skeleton className="w-[193px] h-[400px] rounded-lg" />
-      <Skeleton className="w-[193px] h-[400px] rounded-lg" />
-      <Skeleton className="w-[193px] h-[400px] rounded-lg" />
-      <Skeleton className="w-[193px] h-[400px] rounded-lg" />
-      <Skeleton className="w-[193px] h-[400px] rounded-lg" />
-      <Skeleton className="w-[193px] h-[400px] rounded-lg" />
-      <Skeleton className="w-[193px] h-[400px] rounded-lg" />
-      <Skeleton className="w-[193px] h-[400px] rounded-lg" />
-      <Skeleton className="w-[193px] h-[400px] rounded-lg" />
-      <Skeleton className="w-[193px] h-[400px] rounded-lg" />
+      {Array.from({ length: 10 }, (_, index) => (
+        <Skeleton key={index} className="w-[193px] h-[400px] rounded-lg" data-testid="skeleton" />
+      ))}
     </div>
   );
 };
